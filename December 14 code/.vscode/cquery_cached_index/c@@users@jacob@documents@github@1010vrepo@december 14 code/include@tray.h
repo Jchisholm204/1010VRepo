aae86dbee284e1bf89e2f3196ef7a04ca64688cf
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2019-12-12 17:27:55 -0800

    from desktop

--- December 14 code/.vscode/cquery_cached_index/c@@users@jacob@documents@github@1010vrepo@december 14 code/include@tray.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRAY_H_
-#define TRAY_H_
-
-class Tray{
-public:
-  void opTray(void); //operator tray function
-  void pid(int target, int timeout, float kP, float kI, float kD); //tray stack function
-};
-#endif
