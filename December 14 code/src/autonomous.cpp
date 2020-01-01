166dcc1ef24525081e1fe33e708ed618b4f2b472
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-01-01 12:04:49 -0800

    add cuebstack skills/remove .vs

--- December 14 code/src/autonomous.cpp
+++ December 14 code/src/autonomous.cpp
@@ -90,15 +90,49 @@ void blue2(){
   delay(1500);
   driveF.timeDrive(1000, -50);
 }
+void cubestackSKILLS(){
+  intake.intake(200);
+  driveF.MoveSlow(DIRECTION_FORWARD, 900, 90, 4000);
+  driveF.angleDrive(1, -200, 2200);
+  driveF.MoveSlow(DIRECTION_FORWARD, 940, 90, 2500);
+  driveF.MoveSlow(DIRECTION_BACK, 630, 100, 3000);
+  driveF.TurnDistance(DIRECTION_RIGHT, 920, 50, 1000);
+  intake.intake(-50);
+  driveF.timeGlide(900, 70); //was 600 was 700
+  driveF.stopDriving();
+  intake.intake(0);
+  driveF.MoveSlow(DIRECTION_FORWARD, 270, 40, 1000);
+  driveF.stopDriving();
+  //stacky.move_absolute(TRAYUP);
+  cubeTray.pid(530, 3000, 0.4, 0, 1.2);
+  intake.intake(-50);
+  pros::delay(200);
+  driveF.timeDrive(750, -30);
+  stacky.move_absolute(0, 100);
+  driveF.MoveSlow(DIRECTION_BACK, 100, 40, 1000);
+  intake.intake(200);
+  driveF.TurnDistance(DIRECTION_LEFT, 1000, 40, 2000);
+  driveF.MoveSlow(DIRECTION_FORWARD, 800, 40, 3500);
+  intake.intake(-20);
+  arm.proLift(500, 1000);
+  intake.intake(-100);
+  driveF.MoveSlow(DIRECTION_BACK, 200, 40, 3000);
+
+  //cubeTray.stack(800, 2000);
+
+
+}
 //autonvoid
 void autonomous() {
   expand();
-  cubestackBLUE();
+  //cubestackRED();
+  //cubestackSKILLS();
   //cubeTray.pid(510, 3000, 0.8, 0, 1.2);
   //Display
   //pros::lcd::print(1, "VOID: AUTON");
   //flipout();
   //skerewit();
+  //
   //mechautored();
   //playoffsred();
   //cubestackRED();
