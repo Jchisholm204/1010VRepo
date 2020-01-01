61c6bd530258e10df987c5f2d85c937fc239ca3c
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-01-01 12:00:02 -0800

    delete .vs

--- December 14 code/.vscode/cquery_cached_index/c@@users@jacob@onedrive@desktop@github@1010vrepo@december 14 code/include@diagnostics.h
+++
@@ -1,4 +0,0 @@
-#ifndef _DIAGNOSTICS_H_
-#define _DIAGNOSTICS_H_
-void diagnostics_fn(void*parameter);
-#endif
--- December 14 code/.vscode/cquery_cached_index/c@@users@jacob@onedrive@desktop@github@1010vrepo@december 14 code/include@intakes.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTAKES_H_
-#define INTAKES_H_
-
-class Intake{
-public:
-  void opintake(void);
-  void intake(int speed);
-};
-#endif
