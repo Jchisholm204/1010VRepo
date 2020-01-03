478f427521f26512903888262568bec9cd7def5c
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-01-02 16:32:21 -0800

    add telop switches

--- postBreak/src/lift.cpp
+++ postBreak/src/lift.cpp
@@ -13,39 +13,43 @@ void ArmLift_fn(void*param){
   bool TrayZero;
 
   while(true){
+    if(partner.get_digital(E_CONTROLLER_DIGITAL_L2)){
+      ArmMotor.move_velocity(partner.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
+      if(partner.get_digital(E_CONTROLLER_DIGITAL_L1)){ArmMotor.tare_position();}
+    }
+    else{
+      switch(liftState){
+        case armDown:
+          targetValue = 0;
+          break;
 
-    switch(liftState){
-      case armDown:
-        targetValue = 0;
-        break;
+        case armLow:
+          targetValue = 200;
+          break;
 
-      case armLow:
-        targetValue = 200;
-        break;
+        case armHigh:
+          targetValue = 300;
+          break;
 
-      case armHigh:
-        targetValue = 300;
-        break;
+        case armDeScore:
+          targetValue = 150;
+          break;
 
-      case armDeScore:
-        targetValue = 150;
-        break;
+        default:
+          liftState = 0;
+          break;
 
-      default:
-        liftState = 0;
-        break;
+      };
 
-    };
+      currentValue = ArmMotor.get_position();
+      err = targetValue - currentValue; //error is delta of target and current positions
+      err_last = err; //store last error
+      derr = err - err_last; //difference of errors over iterations
+      motorPower = (0.9 * err) + (1.6 * derr); //PD constants plus our variables
+      motorPower = motorPower > 127 ? 127 : motorPower < -127 ? -127 : motorPower; //caps output at +127, -127
+   //motorPower > cap ?  cap :
 
-    currentValue = ArmMotor.get_position();
-    err = targetValue - currentValue; //error is delta of target and current positions
-    err_last = err; //store last error
-    derr = err - err_last; //difference of errors over iterations
-    motorPower = (0.9 * err) + (1.6 * derr); //PD constants plus our variables
-    motorPower = motorPower > 127 ? 127 : motorPower < -127 ? -127 : motorPower; //caps output at +127, -127
- //motorPower > cap ?  cap :
-
-    ArmMotor.move(motorPower); //move the lift equal to motorPower
-
+      ArmMotor.move(motorPower); //move the lift equal to motorPower
+    }
   }
 }
--- postBreak/src/tray.cpp
+++ postBreak/src/tray.cpp
@@ -19,16 +19,16 @@ void trayPreset_fn(void*param){
  int cap;
  int derr;
  int trayTarget;
- bool TrayZero;
+ bool TrayZero = false;
+ bool TrayOP;
 
  while(true){
+   if(competition::is_autonomous()){TrayOP = false;}
+   else{TrayOP = true;}
 
    currentValue = abs(TrayPos.get_value_calibrated());  //get value from trayPent --- changed from tray motor encoder for improved accuracy
 
-   if(competition::is_autonomous()){
-     targetValue = TrayAutoTarget; //autonomous control is a variable changed in autonomous.cpp
-   }
-   else if(TrayZero == true){
+   if(TrayZero == true){
      while(TrayDownLimit.get_value() == false){
        TrayMotor.move_velocity(-80);
      }
@@ -36,40 +36,47 @@ void trayPreset_fn(void*param){
      TrayMotor.tare_position();
      TrayPos.calibrate();
    }
-   else{
+   else if (TrayOP == true){
 
      int LIFTEXP = expo(partner.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y), 1.5 /*DriveExp*/, 8 /*JoyDead*/, 15 /*MotorMin*/); //tray expo
 
      if(partner.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y) > 0 &&! TrayUpLimit.get_value()){ //if the joystick is above 0 (moving up); watch to see that the up limit isnt toggled
         TrayMotor.move_velocity(LIFTEXP); //move the tray motor with the expo output
+        trayTarget = abs(TrayPos.get_value_calibrated());
       }
       else if(partner.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y) < 0 &&! TrayDownLimit.get_value()){ //if the joystick is in the down position; stop the action if the down switch it toggled
         TrayMotor.move_velocity(LIFTEXP); //move the tray motor with the expo output
+        trayTarget = abs(TrayPos.get_value_calibrated());
       }
-   };
-
-   if((currentValue > 350)){ //if  we're past appx. halfway
-     cap = 50; //slow down so we don't knock down the tower
+      else{
+        trayTarget = abs(TrayPos.get_value_calibrated());
+      }
    }
-   else{
-     cap = 127; //otherwise move normally
-   };
+   else if(competition::is_autonomous()){
+     targetValue = TrayAutoTarget;
 
-   err = targetValue - currentValue; //error is delta of target and current positions
-   err_last = err; //store last error
-   derr = err - err_last; //difference of errors over iterations
-   motorPower = (0.7 * err) + (1.35 * derr); //PD constants plus our variables
-   motorPower = motorPower > cap ?  cap : motorPower < -127 ? -127 : motorPower; //caps output at +cap, -127
+     if((currentValue > 350)){ //if  we're past appx. halfway
+       cap = 50; //slow down so we don't knock down the tower
+     }
+     else{
+       cap = 127; //otherwise move normally
+     };
 
-   if((TrayUpLimit.get_value() == HIGH)){  //if our active stop variable is true
-     TrayMotor.move(0); //stop movement, to prevent the motor from overheating
-   }
-   else{
-     TrayMotor.move(motorPower); //move the lift equal to motorPower
-   }
+     err = targetValue - currentValue; //error is delta of target and current positions
+     err_last = err; //store last error
+     derr = err - err_last; //difference of errors over iterations
+     motorPower = (0.7 * err) + (1.35 * derr); //PD constants plus our variables
+     motorPower = motorPower > cap ?  cap : motorPower < -127 ? -127 : motorPower; //caps output at +cap, -127
+
+     if((TrayUpLimit.get_value() == HIGH)){  //if our active stop variable is true
+       TrayMotor.move(0); //stop movement, to prevent the motor from overheating
+     }
+     else{
+       TrayMotor.move(motorPower); //move the lift equal to motorPower
+     }
  }
 }
-
+}
 
 /*
 CREDIT:
