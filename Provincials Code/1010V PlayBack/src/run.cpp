aee5b0c325e2b5c6b884efd73568af32c9ae681d
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-01-30 23:03:02 -0800

    runfile changed to blue up

--- Provincials Code/1010V PlayBack/src/run.cpp
+++ Provincials Code/1010V PlayBack/src/run.cpp
@@ -12,7 +12,7 @@ EX:
 fp = fopen("/usd/UpBLUE.txt", "r");
 Would run the unprotected blue recording
   */
-  fp = fopen("/usd/example.txt", "r"); //opens the file with the auton in it
+  fp = fopen("/usd/UpBLUE.txt", "r"); //opens the file with the auton in it
 
   static float m1, m2, m3, m4, m5, m6, m7, m8; //these are placeholders for motor volatges
 
