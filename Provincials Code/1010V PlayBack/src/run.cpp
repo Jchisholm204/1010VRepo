477642de9ad360e31ad7aca9aee132a5bf10600e
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-02-03 12:50:34 -0800

    add read from armfile

--- Provincials Code/1010V PlayBack/src/run.cpp
+++ Provincials Code/1010V PlayBack/src/run.cpp
@@ -1,24 +1,34 @@
 #include "main.h"
+#include "chassis.h"
+#include "Intakes.h"
+#include "tray.h"
+#include "lift.h"
 #include "run.h"
 
 void autoRun(void){
 
-  FILE *fp; //create file header fp
+  FILE* mvfile = fopen("/usd/mvstorage.txt", "w");
+  FILE* armf = fopen("/usd/armfile.txt", "w");
+  FILE* intakef = fopen("/usd/intake.txt", "w");
+  FILE* typefile = fopen("/usd/namefile.txt", "w");
   /*
 The fopen statement here is used to open the autonomous file previously recorded to the SD card.
 I typicaly record my files acording to the format "UpRED" for unprotected red, or "pBLUE" for protected blue
 In PROS, files are accessed through /usd/filename.txt
 EX:
-fp = fopen("/usd/UpBLUE.txt", "r");
+mvfile = fopen("/usd/UpBLUE.txt", "r");
 Would run the unprotected blue recording
   */
-  fp = fopen("/usd/example.txt", "r"); //opens the file with the auton in it
-
   static float m1, m2, m3, m4, m5, m6, m7, m8; //these are placeholders for motor volatges
+  static float pres;
+  static float nameboi;
+
+  fscanf(typefile, "%f", &nameboi);
+  master.print(0, 0, "%f", nameboi);
 
   //Auton while loop
   while (true) {
-    if(feof(fp)){  //if end of file reached
+    if(feof(mvfile)){  //if end of file reached
     /////////stop all of the motors
       //drive motors
         driveRB.move_voltage(0);
@@ -33,15 +43,21 @@ Would run the unprotected blue recording
       //arm motor
         ArmMotor.move_voltage(0);
     ///////close the file
-        fclose(fp);
+        fclose(mvfile);
+        fclose(armf);
+        fclose(intakef);
+        fclose(typefile);
         delay(100);
     }
 //reading from the array
-    fscanf(fp, "%f %f %f %f %f %f %f %f", &m1, &m2, &m3, &m4, &m5, &m6, &m7, &m8);
+    fscanf(mvfile, "%f %f %f %f %f %f %f", &m1, &m2, &m3, &m4, &m5, &m6, &m7);
+    fscanf(armf, "%f", &pres);
+
+    liftState = pres;
 
 //moving motors using array values
 //drive motors
-  printf("%f\n", m1);
+  //printf("%f\n", m1);
   driveRB.move_voltage(m1);
   driveRF.move_voltage(m2);
   driveLB.move_voltage(m3);
@@ -52,7 +68,7 @@ Would run the unprotected blue recording
   LeftIntake.move_voltage(m6);
   RightIntake.move_voltage(m7);
 //arm motor
-  ArmMotor.move_voltage(m8);
+  //ArmMotor.move_voltage(m8);
   delay(15);
 }
 }
