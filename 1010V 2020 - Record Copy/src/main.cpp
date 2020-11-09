ad66ba3a14e61b6fdd6af5ac4a68d681de499730
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-11-09 11:30:05 -0800

    add back display for making autos

--- 1010V 2020 - Record Copy/src/main.cpp
+++ 1010V 2020 - Record Copy/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "drive.h"
+#include "display.h"
 
 Controller master(E_CONTROLLER_MASTER);
 Motor driveRB(11, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
@@ -14,15 +15,24 @@ Motor roller(101, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
 Motor flyWheel(101, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 
 Chassis drivef;
+Display display;
 
 void initialize() {
 	driveLF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveLB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveRF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveRB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+
+	display.createScreen();
+	display.refresh();
 }
 
-void disabled() {}
+void disabled() {
+	while(true){
+		display.refresh();
+		delay(100);
+	}
+}
 
 void competition_initialize() {}
 
@@ -38,6 +48,7 @@ void opcontrol() {
 	int timer = 0;
 	FILE* file = fopen("/usd/record.txt", "w");
 	while (timer < 14500) {
+		display.refresh();
 		/////////////////////////DATA COLECTION
 				//////drive
 		fprintf(file, "%f\n", getVelocity(driveRB));
