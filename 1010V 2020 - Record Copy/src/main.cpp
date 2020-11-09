8204ab6c590565f6132476f92e4f6901eba4fe21
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-11-09 10:57:21 -0800

    remove unnessasary files from the Record Copy
    Some of the files in the record copy are unnessasary as we dont need a display, vision or the runfule in order to record autos

--- 1010V 2020 - Record Copy/src/main.cpp
+++ 1010V 2020 - Record Copy/src/main.cpp
@@ -1,8 +1,5 @@
 #include "main.h"
 #include "drive.h"
-#include "display.h"
-#include "vision.h"
-#include "run.h"
 
 Controller master(E_CONTROLLER_MASTER);
 Motor driveRB(11, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
@@ -17,17 +14,12 @@ Motor roller(101, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
 Motor flyWheel(101, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 
 Chassis drivef;
-Display display;
 
 void initialize() {
 	driveLF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveLB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveRF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
 	driveRB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-
-	display.createScreen();
-	display.refresh();
-	//visionINT();
 }
 
 void disabled() {}
@@ -36,16 +28,44 @@ void competition_initialize() {}
 
 
 void autonomous() {
-	autoRun();
+
 }
 
+double getVelocity(Motor motor){
+	return motor.get_actual_velocity();
+}
 void opcontrol() {
-	//display.setActiveTab(op_tab);
-	while (true) {
-		printf("%d\n",SelectedAuto );
+	int timer = 0;
+	FILE* file = fopen("/usd/record.txt", "w");
+	while (timer < 14500) {
+		/////////////////////////DATA COLECTION
+				//////drive
+		fprintf(file, "%f\n", getVelocity(driveRB));
+		fprintf(file, "%f\n", getVelocity(driveRF));
+		fprintf(file, "%f\n", getVelocity(driveLB));
+		fprintf(file, "%f\n", getVelocity(driveLF));
+				////roller
+		fprintf(file, "%f\n", getVelocity(roller));
+				///intakes
+		fprintf(file, "%f\n", getVelocity(intakeR));
+		fprintf(file, "%f\n", getVelocity(intakeL));
+				////fast spinning roller
+		fprintf(file, "%f\n", getVelocity(flyWheel));
 		drivef.operator_Chassis();
-		display.refresh();
-		pros::delay(20);
-		//visionLoop();
+
+		delay(15);
+		timer += 15;
+
+		if(timer > 14500){
+			driveLB.move(0);
+			driveLF.move(0);
+			driveRB.move(0);
+			driveRF.move(0);
+			flyWheel.move(0);
+			roller.move(0);
+			intakeR.move(0);
+			intakeL.move(0);
+		}
 	}
+		fclose(file);
 }
