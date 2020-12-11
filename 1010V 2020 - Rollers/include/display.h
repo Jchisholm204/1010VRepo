494da46f5f4e2f5157f2727b4f4ba62a03af9dd7
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2020-12-11 10:08:46 -0800

    Create

---
+++ 1010V 2020 - Rollers/include/display.h
@@ -0,0 +1,25 @@
+#ifndef _LCD_H_
+#define _LCD_H_
+#include <stdio.h>
+#include <errno.h>
+
+
+#define NO_AUTO_SELECTED 0
+extern int SelectedAuto;
+
+
+
+class Display{
+public:
+  void createTabs(void);
+  void setActiveTab(int tab);
+  void createScreen(void);
+  void createOpLEDs(void);
+  void createTitle(void);
+  void createClawIndicators(void);
+  void createAutoSelector(void);
+  void createBatteryMeter(void);
+  void refresh(void);
+};
+
+#endif
