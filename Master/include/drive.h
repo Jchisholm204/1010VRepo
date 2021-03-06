8d05a8ccefacc1a28f7531b5e82dc3275a057b79
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2021-03-05 20:05:01 -0800

    Removed speed variable from move functions

--- Master/include/drive.h
+++ Master/include/drive.h
@@ -1,11 +1,12 @@
 #ifndef _DRIVE_H_
 #define _DRIVE_H_
+
 class Chassis{
 public:
   void operator_Chassis(void);
   void timeDrive(int time/*the time it drives for*/, int vel/*the entered Speed*/, int cooldown/*the period that the robot should drive for a slower % of the speed*/);
-  void MoveDistance(int direction, int targetValue, int speed, int timeout);
-  void TurnDistance(int direction, int targetValue, int speed, int timeout);
+  void MoveDistance(int direction, int targetValue, int timeout);
+  void TurnDistance(int direction, int targetValue, int timeout);
   //void TurnGyro(int direction, int targetValue, int timeout);
   void stopDriving(void);
   void startDriving(int speed);
