54956390428291b2cf2be5faab930adaa7938682
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2021-12-07 09:05:26 -0800

    change end early key to RIGHT

--- 1010B - 2021 ReRun One File/src/Autons/ReRun.cpp
+++ 1010B - 2021 ReRun One File/src/Autons/ReRun.cpp
@@ -152,7 +152,7 @@ int recordAuto(int reRunFile, bool recording_disabled, int allottedTime){
         pros::delay(rec_loop_delay);
 
         //EndEarly Function - allows auto to be cut short of the allotted time
-        if(master.get_digital(E_CONTROLLER_DIGITAL_DOWN)){
+        if(master.get_digital(E_CONTROLLER_DIGITAL_RIGHT)){
             endEarly = true;
         }
         //Ends while loop and immediately closes file
