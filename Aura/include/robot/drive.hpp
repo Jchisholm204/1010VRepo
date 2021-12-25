a49352158d892e109f58c1b3dace219eb7403651
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2021-12-25 11:44:48 -0800

    documentation

--- Aura/include/robot/drive.hpp
+++ Aura/include/robot/drive.hpp
@@ -13,15 +13,46 @@ class Chassis{
 public:
   //exponential drive function
   int exponential(int joystickVal, float driveExp, int joydead, int motorMin);
-  //operator drive
+
+  /**
+   * @brief Operator Chassis using Arcade Drive With Expo
+   * 
+   * @param maxVel The Maximum Velocity The Drivebase Can Run (0-200)
+   */
   void operator_Chassis(int maxVel);
-  //time driven drivebase movement with different drive powers
+
+  /**
+   * @brief Time Driven Drivebase Movement with Different Drive Velocities
+   * 
+   * @param time The Time To Drive For
+   * @param leftPow The Velocity of The Left Side of The Robot
+   * @param rightPow The Velocity of The Right Side of The Robot
+   */
   void time(int time, int leftPow, int rightPow);
-  //time driven drivebase movement
+  /**
+   * @brief Time Driven Drivebase Movement
+   * 
+   * @param time The Time To Drive For
+   * @param velocity The Velocity of All 4 drive motors
+   */
   void time(int time, int velocity);
-  //encoder based drivebase movement with left and right pid values
+  /**
+   * @brief Encoder Based Drivebase Movement With Left and Right PID Values
+   * 
+   * @param leftTarget The Left Side PID Target
+   * @param maxLeft The Maximum Power (Speed) That Can be Applied to the Left Side
+   * @param rightTarget The Right Side PID Target
+   * @param maxRight The Maximum Power (Speed) That Can be Applied to the Right Side
+   * @param timeout The PID Timeout in ms
+   */
   void driveTurn(int leftTarget, int maxLeft, int rightTarget, int maxRight, int timeout);
-  //gyro based drivebase turning with center origins
+  
+  /**
+   * @brief Gyro Based Drivebase Turning with Center Origins
+   * 
+   * @param targetValue The Degrees to Turn from its Current Position
+   * @param timeout The PID Timeout in ms
+   */
   void turn(int targetValue, int timeout);
   //pid
   void pid(int targetValue, int maxSpeed, int timeout, float kP, float kD);
